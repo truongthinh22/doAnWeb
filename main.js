cc94ca39e2ec706f2d5196e32748cf40211d0536
truongthinh <2100011758@nttu.edu.vn>
2022-08-30 17:37:08 +0700

    upload wweb

---
+++ main.js
@@ -0,0 +1,26 @@
+﻿// JavaScript source code
+// js-khi đưa chuột vào vùng hienLen thì sẽ hiện lên div có class no1-no2
+$(document).ready(function () {
+    $(".hienLen1").on({
+        mouseenter: function () {
+            $(".no1").fadeIn();
+        },
+        mouseenter: function () {
+            $(".no1").css("display", "contents");
+        },
+        mouseleave: function () {
+            $(".no1").fadeOut();
+        }
+    });
+    $(".hienLen2").on({
+        mouseenter: function () {
+            $(".no2").fadeIn();
+        },
+        mouseenter: function () {
+            $(".no2").css("display", "contents");
+        },
+        mouseleave: function () {
+            $(".no2").fadeOut();
+        }
+    });
+});
